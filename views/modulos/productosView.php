2ea643b279ac6f126027c0c895cb7002b332eb7c
José L. Gutiérrez <gutiecuador@gmail.com>
2021-11-10 19:01:50 -0500

    productos participantes

--- views/modulos/productosView.php
+++ views/modulos/productosView.php
@@ -1,21 +1,120 @@
-libs/<?php require_once 'sis_modules/navbar.php' ?>
+<?php require_once 'sis_modules/navbar.php' ?>
 
 
     <div class="main-container main-background">
         <div class="fireworks">
-            <section style="z-index: 2; height: 600px;" class="text-light">
+            <section style="z-index: 2;" class="text-light">
                 <div class="container">
                     
-                    <div class="row justify-content-center mb-4">
-                        
+                <div class="row justify-content-center">
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center">
+                            <img class="card-img-top" style="max-width: 100px;" src="assets/img/BotellaRonDorado375.png" alt="Producto">
+                            <div class="card-body">
+                            <h4 class="card-title  text-dark">Ron Medellín Dorado 375ml</h4>
+                            <p class="card-text" style="font-size:0.8rem">Un Ron versátil compuesto de una mezcla entre 2 y 3 años que gracias a su añejamiento en barriles de Roble Americano logra unas propiedades únicas. Un sabor dulce con notas de vainilla, caramelo, chocolate, frutos secos y miel que deja una sensación sedosa en la boca. Un Ron de sabor suave ideal para cada ocasión.</p>
+                            </div>
+                        </div>
                     </div>
+                    <!--end of col-->
 
-                    
-
-                
-                
-                    <!--end of row-->
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center">
+                            <img class="card-img-top" style="max-width: 124px;" src="assets/img/BotellaRonDorado750.png" alt="Producto">
+                            <div class="card-body">
+                            <h4 class="card-title  text-dark">Ron Medellín Dorado 750ml</h4>
+                            <p class="card-text" style="font-size:0.8rem">Un Ron versátil compuesto de una mezcla entre 2 y 3 años que gracias a su añejamiento en barriles de Roble Americano logra unas propiedades únicas. Un sabor dulce con notas de vainilla, caramelo, chocolate, frutos secos y miel que deja una sensación sedosa en la boca. Un Ron de sabor suave ideal para cada ocasión.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
                 </div>
+
+
+                <div class="row justify-content-center">
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center">
+                            <img class="card-img-top" style="max-width: 100px;" src="assets/img/BotellaRon375.png" alt="Producto">
+                            <div class="card-body">
+                            <h4 class="card-title  text-dark">Ron Medellín 3 años Añejo 375ml</h4>
+                            <p class="card-text" style="font-size:0.8rem">Un trago potente y con carácter, proveniente de la mezcla de rones de 3 y 4 años. Su sabor con notas a madera y vainilla lo obtiene directamente del barril y lo convierte en un complemento ideal para preparar cocteles.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center">
+                            <img class="card-img-top" style="max-width: 100px;" src="assets/img/BotellaRon750.png" alt="Producto">
+                            <div class="card-body">
+                            <h4 class="card-title  text-dark">Ron Medellín 3 años Añejo 750ml</h4>
+                            <p class="card-text" style="font-size:0.8rem">Un trago potente y con carácter, proveniente de la mezcla de rones de 3 y 4 años. Su sabor con notas a madera y vainilla lo obtiene directamente del barril y lo convierte en un complemento ideal para preparar cocteles.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center">
+                            <img class="card-img-top" style="max-width: 100px;" src="assets/img/BotellaRon1000.png" alt="Producto">
+                            <div class="card-body">
+                            <h4 class="card-title  text-dark">Ron Medellín 3 años Añejo 1000ml</h4>
+                            <p class="card-text" style="font-size:0.8rem">Un trago potente y con carácter, proveniente de la mezcla de rones de 3 y 4 años. Su sabor con notas a madera y vainilla lo obtiene directamente del barril y lo convierte en un complemento ideal para preparar cocteles.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+
+
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center">
+                            <img class="card-img-top" style="max-width: 100px;" src="assets/img/ROJO375.png" alt="Producto">
+                            <div class="card-body">
+                            <h4 class="card-title  text-dark">Aguardiente Antioqueño Tradicional (Tapa Roja)</h4>
+                            <p class="card-text" style="font-size:0.8rem">
+                            Un Aguardiente con notas dulces y alcohólicas ligeramente suaves que al mezclarse resaltan el sabor característico de nuestras tradiciones. Es la referencia de la familia para aquellos consumidores de siempre o quienes buscan un trago un poco más fuerte.
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center">
+                            <img class="card-img-top" style="max-width: 100px;" src="assets/img/AZUL375.png" alt="Producto">
+                            <div class="card-body">
+                            <h4 class="card-title  text-dark">guardiente Antioqueño Sin Azúcar (Tapa Azul)</h4>
+                            <p class="card-text" style="font-size:0.8rem">
+                            Para quienes se toman los traguitos sin remordimiento. Una fórmula con notas alcohólicas suavizadas que logran un cálido e incomparable sabor y aroma, manteniendo los 29° de Vol. de alcohol pero con el beneficio de ser Sin Azúcar.
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center">
+                            <img class="card-img-top" style="max-width: 100px;" src="assets/img/VERDE375.png" alt="Producto">
+                            <div class="card-body">
+                            <h4 class="card-title  text-dark">Aguardiente  Antioqueño 24° Sin Azúcar (Tapa Verde)</h4>
+                            <p class="card-text" style="font-size:0.8rem">
+                                Un Aguardiente más suave que te permite alargar todos esos momentos felices. Un trago con notas dulces y ligeramente suave que le dan un gran balance entre sabor y aroma. Más suave + horas de fiesta
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+
+                   
+
+                </div>
+
+
+
+               
             <!--end of container-->
             </section>
         </div>
