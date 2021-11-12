dfeef432bd10eb9c2d585b870dc99e59b6e8d218
José L. Gutiérrez <gutiecuador@gmail.com>
2021-11-11 20:53:32 -0500

    update productos participantes

--- views/modulos/productosView.php
+++ views/modulos/productosView.php
@@ -5,14 +5,17 @@
         <div class="fireworks">
             <section style="z-index: 2;" class="text-light">
                 <div class="container">
-                    
+
+                <h2 class="text-center text-white mb-3" style="color: #ffeb00 !important; font-weight: bold; font-size: 2rem;">
+                Ron Medellín Dorado 375ml & 750ml
+                </h2>
+           
                 <div class="row justify-content-center">
                     <div class="col-12 col-sm-6 col-md-4">
-                        <div class="card text-center text-dark" style="align-items: center">
+                        <div class="card text-center text-dark" style="align-items: center; display:block">
                             <img class="card-img-top" style="max-width: 100px;" src="assets/img/BotellaRonDorado375.png" alt="Producto">
                             <div class="card-body">
-                            <h4 class="card-title  text-dark">Ron Medellín Dorado 375ml</h4>
-                            <p class="card-text" style="font-size:0.8rem">Un Ron versátil compuesto de una mezcla entre 2 y 3 años que gracias a su añejamiento en barriles de Roble Americano logra unas propiedades únicas. Un sabor dulce con notas de vainilla, caramelo, chocolate, frutos secos y miel que deja una sensación sedosa en la boca. Un Ron de sabor suave ideal para cada ocasión.</p>
+                                <h4 class="card-title  text-dark">Ron Medellín Dorado 375ml</h4>
                             </div>
                         </div>
                     </div>
@@ -22,44 +25,63 @@
                         <div class="card text-center text-dark" style="align-items: center">
                             <img class="card-img-top" style="max-width: 124px;" src="assets/img/BotellaRonDorado750.png" alt="Producto">
                             <div class="card-body">
-                            <h4 class="card-title  text-dark">Ron Medellín Dorado 750ml</h4>
-                            <p class="card-text" style="font-size:0.8rem">Un Ron versátil compuesto de una mezcla entre 2 y 3 años que gracias a su añejamiento en barriles de Roble Americano logra unas propiedades únicas. Un sabor dulce con notas de vainilla, caramelo, chocolate, frutos secos y miel que deja una sensación sedosa en la boca. Un Ron de sabor suave ideal para cada ocasión.</p>
+                                <h4 class="card-title  text-dark">Ron Medellín Dorado 750ml</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center; height: 94%;">
+                            <div class="card-body" style="font-size: 1.2rem;">
+                            
+                            <p class="card-text" >Un Ron versátil compuesto de una mezcla entre 2 y 3 años que gracias a su añejamiento en barriles de Roble Americano logra unas propiedades únicas. Un sabor dulce con notas de vainilla, caramelo, chocolate, frutos secos y miel que deja una sensación sedosa en la boca. Un Ron de sabor suave ideal para cada ocasión.</p>
                             </div>
                         </div>
                     </div>
                     <!--end of col-->
                 </div>
 
+                <h2 class="text-center text-white mb-3" style="color: #ffeb00 !important; font-weight: bold; font-size: 2rem;">
+                Ron Medellín 3 años 375ml, 750ml & 1000ml
+                </h2>
 
                 <div class="row justify-content-center">
-                    <div class="col-12 col-sm-6 col-md-4">
+                
+                    <div class="col-12 col-sm-6 col-md-3">
                         <div class="card text-center text-dark" style="align-items: center">
                             <img class="card-img-top" style="max-width: 100px;" src="assets/img/BotellaRon375.png" alt="Producto">
                             <div class="card-body">
-                            <h4 class="card-title  text-dark">Ron Medellín 3 años Añejo 375ml</h4>
-                            <p class="card-text" style="font-size:0.8rem">Un trago potente y con carácter, proveniente de la mezcla de rones de 3 y 4 años. Su sabor con notas a madera y vainilla lo obtiene directamente del barril y lo convierte en un complemento ideal para preparar cocteles.</p>
+                                <h4 class="card-title  text-dark">Ron Medellín 3 años Añejo 375ml</h4>
                             </div>
                         </div>
                     </div>
                     <!--end of col-->
 
-                    <div class="col-12 col-sm-6 col-md-4">
+                    <div class="col-12 col-sm-6 col-md-3">
                         <div class="card text-center text-dark" style="align-items: center">
-                            <img class="card-img-top" style="max-width: 100px;" src="assets/img/BotellaRon750.png" alt="Producto">
+                            <img class="card-img-top" style="max-width: 105px;" src="assets/img/BotellaRon750.png" alt="Producto">
                             <div class="card-body">
-                            <h4 class="card-title  text-dark">Ron Medellín 3 años Añejo 750ml</h4>
-                            <p class="card-text" style="font-size:0.8rem">Un trago potente y con carácter, proveniente de la mezcla de rones de 3 y 4 años. Su sabor con notas a madera y vainilla lo obtiene directamente del barril y lo convierte en un complemento ideal para preparar cocteles.</p>
+                                <h4 class="card-title  text-dark">Ron Medellín 3 años Añejo 750ml</h4>
                             </div>
                         </div>
                     </div>
                     <!--end of col-->
 
-                    <div class="col-12 col-sm-6 col-md-4">
+                    <div class="col-12 col-sm-6 col-md-3">
                         <div class="card text-center text-dark" style="align-items: center">
-                            <img class="card-img-top" style="max-width: 100px;" src="assets/img/BotellaRon1000.png" alt="Producto">
+                            <img class="card-img-top" style="max-width: 108px;" src="assets/img/BotellaRon1000.png" alt="Producto">
                             <div class="card-body">
-                            <h4 class="card-title  text-dark">Ron Medellín 3 años Añejo 1000ml</h4>
-                            <p class="card-text" style="font-size:0.8rem">Un trago potente y con carácter, proveniente de la mezcla de rones de 3 y 4 años. Su sabor con notas a madera y vainilla lo obtiene directamente del barril y lo convierte en un complemento ideal para preparar cocteles.</p>
+                                <h4 class="card-title  text-dark">Ron Medellín 3 años Añejo 1000ml</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+
+                    <div class="col-12 col-sm-6 col-md-3">
+                        <div class="card text-center text-dark" style="align-items: center; height:94%">
+                            <div class="card-body">
+                            <p class="card-text" style="font-size:1.2rem">Un trago potente y con carácter, proveniente de la mezcla de rones de 3 y 4 años. Su sabor con notas a madera y vainilla lo obtiene directamente del barril y lo convierte en un complemento ideal para preparar cocteles.</p>
                             </div>
                         </div>
                     </div>
@@ -68,28 +90,53 @@
 
                 </div>
 
+                <h2 class="text-center text-white mb-3" style="color: #ffeb00 !important; font-weight: bold; font-size: 2rem;">
+                    Aguardiente Antioqueño Tradicional 375ml & 750ml
+                </h2>
+
                 <div class="row justify-content-center">
                     <div class="col-12 col-sm-6 col-md-4">
-                        <div class="card text-center text-dark" style="align-items: center">
-                            <img class="card-img-top" style="max-width: 100px;" src="assets/img/ROJO375.png" alt="Producto">
+                        <div class="card text-center text-dark" style="align-items: center; display:block">
+                            <img class="card-img-top" style="max-width: 200px;" src="assets/img/ROJO375.png" alt="Producto">
                             <div class="card-body">
-                            <h4 class="card-title  text-dark">Aguardiente Antioqueño Tradicional (Tapa Roja)</h4>
-                            <p class="card-text" style="font-size:0.8rem">
+                                <h4 class="card-title  text-dark">Aguardiente Antioqueño Tradicional (Tapa Roja) 375ml</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center">
+                            <img class="card-img-top" style="max-width: 124px;" src="assets/img/ROJO750.png" alt="Producto">
+                            <div class="card-body">
+                                <h4 class="card-title  text-dark">Aguardiente Antioqueño Tradicional (Tapa Roja) 750ml</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center; height: 94%;">
+                            <div class="card-body">
+                            <p class="card-text" style="font-size: 1.3rem;">
                             Un Aguardiente con notas dulces y alcohólicas ligeramente suaves que al mezclarse resaltan el sabor característico de nuestras tradiciones. Es la referencia de la familia para aquellos consumidores de siempre o quienes buscan un trago un poco más fuerte.
                             </p>
                             </div>
                         </div>
                     </div>
                     <!--end of col-->
+                </div>
 
+                <h2 class="text-center text-white mb-3" style="color: #ffeb00 !important; font-weight: bold; font-size: 2rem;">
+                    Aguardiente Antioqueño Sin Azúcar 375ml & 750ml
+                </h2>
+
+                <div class="row justify-content-center">
                     <div class="col-12 col-sm-6 col-md-4">
-                        <div class="card text-center text-dark" style="align-items: center">
-                            <img class="card-img-top" style="max-width: 100px;" src="assets/img/AZUL375.png" alt="Producto">
+                        <div class="card text-center text-dark" style="align-items: center; display:block">
+                            <img class="card-img-top" style="max-width: 200px;" src="assets/img/AZUL375.png" alt="Producto">
                             <div class="card-body">
-                            <h4 class="card-title  text-dark">guardiente Antioqueño Sin Azúcar (Tapa Azul)</h4>
-                            <p class="card-text" style="font-size:0.8rem">
-                            Para quienes se toman los traguitos sin remordimiento. Una fórmula con notas alcohólicas suavizadas que logran un cálido e incomparable sabor y aroma, manteniendo los 29° de Vol. de alcohol pero con el beneficio de ser Sin Azúcar.
-                            </p>
+                                <h4 class="card-title  text-dark">Aguardiente Antioqueño Sin Azúcar (Tapa Azul) 375ml</h4>
                             </div>
                         </div>
                     </div>
@@ -97,19 +144,61 @@
 
                     <div class="col-12 col-sm-6 col-md-4">
                         <div class="card text-center text-dark" style="align-items: center">
-                            <img class="card-img-top" style="max-width: 100px;" src="assets/img/VERDE375.png" alt="Producto">
+                            <img class="card-img-top" style="max-width: 124px;" src="assets/img/AZUL750.png" alt="Producto">
                             <div class="card-body">
-                            <h4 class="card-title  text-dark">Aguardiente  Antioqueño 24° Sin Azúcar (Tapa Verde)</h4>
-                            <p class="card-text" style="font-size:0.8rem">
+                                <h4 class="card-title  text-dark">Aguardiente Antioqueño Sin Azúcar (Tapa Azul) 750ml</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center; height: 94%;">
+                            <div class="card-body">
+                            <p class="card-text" style="font-size: 1.3rem;">
+                                Para quienes se toman los traguitos sin remordimiento. Una fórmula con notas alcohólicas suavizadas que logran un cálido e incomparable sabor y aroma, manteniendo los 29° de Vol. de alcohol pero con el beneficio de ser Sin Azúcar.
+                            </p>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+                </div>
+
+                <h2 class="text-center text-white mb-3" style="color: #ffeb00 !important; font-weight: bold; font-size: 2rem;">
+                    Aguardiente  Antioqueño 24° Sin Azúcar 375ml & 750ml
+                </h2>
+
+                <div class="row justify-content-center">
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center; display:block">
+                            <img class="card-img-top" style="max-width: 200px;" src="assets/img/VERDE375.png" alt="Producto">
+                            <div class="card-body">
+                                <h4 class="card-title  text-dark">Aguardiente Antioqueño Sin Azúcar (Tapa Azul) 375ml</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center">
+                            <img class="card-img-top" style="max-width: 124px;" src="assets/img/VERDE750.png" alt="Producto">
+                            <div class="card-body">
+                                <h4 class="card-title  text-dark">Aguardiente Antioqueño Sin Azúcar (Tapa Azul) 750ml</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <!--end of col-->
+
+                    <div class="col-12 col-sm-6 col-md-4">
+                        <div class="card text-center text-dark" style="align-items: center; height: 94%;">
+                            <div class="card-body">
+                            <p class="card-text" style="font-size: 1.3rem;">
                                 Un Aguardiente más suave que te permite alargar todos esos momentos felices. Un trago con notas dulces y ligeramente suave que le dan un gran balance entre sabor y aroma. Más suave + horas de fiesta
                             </p>
                             </div>
                         </div>
                     </div>
                     <!--end of col-->
-
-                   
-
                 </div>
 
 
